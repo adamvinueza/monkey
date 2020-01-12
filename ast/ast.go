d016ba3432246bd2e2789e8f538cce85c24c0cde
adamvinueza <adamvinueza@pm.me>
2020-01-12 11:43:03 -0700

    adding string methods in prep for string parsing

--- ast/ast.go
+++ ast/ast.go
@@ -1,12 +1,16 @@
 package ast
 
-import "github.com/adamvinueza/monkey/token"
+import (
+	"bytes"
+	"github.com/adamvinueza/monkey/token"
+)
 
 // Node is the fundamental unit in an abstract syntax tree.
 type Node interface {
 	// TokenLiteral returns the literal representing the lexical token that
 	// represents this Node.
 	TokenLiteral() string
+	String() string
 }
 
 // Statement is a program statement, such as "let x = 5;".
@@ -40,6 +44,14 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // LetStatement represents the structure of a let statement. It is of the form
 // LET NAME = VALUE.
 //
@@ -56,6 +68,24 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+// String returns the string value of this LetStatement.
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ReturnStatement represents the structure of a return statement, such as
+// "return x + y;"
 type ReturnStatement struct {
 	Token       token.Token // the 'return' token
 	ReturnValue Expression
@@ -64,11 +94,42 @@ type ReturnStatement struct {
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ExpressionStatement represents the structure of an expression, considered as
+// a statement. (Expressions are also Statements in Monkey.)
+type ExpressionStatement struct {
+	Token      token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 // Identifier represents the name of a variable.
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
 }
 
-func (i *Identifier) statementNode()       {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
